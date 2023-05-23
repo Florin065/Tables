f19cb22dd9f0f6e505c5d2ba126de2e2bf557777
FLX777 <subtirica.florin29@gmail.com>
2023-05-23 19:40:29 +0300

    Last modified version

--- src/main/scala/Table.scala
+++ src/main/scala/Table.scala
@@ -9,17 +9,17 @@ trait FilterCond {
 }
 case class Field(colName: String, predicate: String => Boolean) extends FilterCond {
   // 2.2.
-  override def eval(r: Row): Option[Boolean] = r.get(colName).map(predicate)
+  override def eval(r: Row): Option[Boolean] = r get colName map predicate
 }
 
 case class And(f1: FilterCond, f2: FilterCond) extends FilterCond {
   // 2.2.
-  override def eval(r: Row): Option[Boolean] = f1.eval(r).flatMap(b1 => f2.eval(r).map(b2 => b1 && b2))
+  override def eval(r: Row): Option[Boolean] = f1 eval r flatMap(b1 => f2 eval r map(b2 => b1 && b2))
 }
 
 case class Or(f1: FilterCond, f2: FilterCond) extends FilterCond {
   // 2.2.
-  override def eval(r: Row): Option[Boolean] = f1.eval(r).flatMap(b1 => f2.eval(r).map(b2 => b1 || b2))
+  override def eval(r: Row): Option[Boolean] = f1 eval r flatMap(b1 => f2 eval r map(b2 => b1 || b2))
 }
 
 trait Query {
@@ -70,16 +70,17 @@ class Table (columnNames: Line, tabular: List[List[String]]) {
   def     getTabular : List[List[String]] =     tabular
 
   // 1.1
-  override def toString: String = (columnNames :: tabular).map(_.mkString(",")).mkString("\n")
+  override def toString: String = (columnNames :: tabular) map(_ mkString ",") mkString "\n"
 
   // 2.1
   def select(columns: Line): Option[Table] = {
-    val newColumnNames = columnNames.filter(columns.contains)
+    val newColumnNames = columnNames filter columns.contains
+
     val newTabular = for {
       row <- tabular
       newValues = for {
         col <- columns
-        index = columnNames.indexOf(col)
+        index = columnNames indexOf col
         if index != -1
       } yield row(index)
     } yield newValues
@@ -90,18 +91,20 @@ class Table (columnNames: Line, tabular: List[List[String]]) {
   // 2.2
   def filter(cond: FilterCond): Option[Table] = cond match {
     case Field(colName, predicate) =>
-      columnNames.indexOf(colName) match {
+      columnNames indexOf colName match {
         case -1 => Option.empty
-        case index => Option(new Table(columnNames, tabular.filter(row => predicate(row(index)))))
+        case index => Option(new Table(columnNames, tabular filter(row => predicate(row(index)))))
       }
+
     case And(f1, f2) =>
-      filter(f1).flatMap(t1 => filter(f2).map(t2 => new Table(t1.getColumnNames, t1.getTabular intersect t2.getTabular)))
+      filter(f1) flatMap(t1 => filter(f2) map(t2 => new Table(t1.getColumnNames, t1.getTabular intersect t2.getTabular)))
+
     case Or(f1, f2) =>
-      filter(f1).flatMap(t1 => filter(f2).map(t2 => new Table(t1.getColumnNames, (t2.getTabular ++ t1.getTabular).distinct)))
+      filter(f1) flatMap(t1 => filter(f2) map(t2 => new Table(t1.getColumnNames, (t2.getTabular ++ t1.getTabular).distinct)))
   }
 
   // 2.3.
-  def newCol(name: String, defaultVal: String): Table = new Table(columnNames :+ name, tabular.map(_ :+ defaultVal))
+  def newCol(name: String, defaultVal: String): Table = new Table(columnNames :+ name, tabular map(_ :+ defaultVal))
 
   // 2.4.
   def merge(key: String, other: Table): Option[Table] = {
@@ -109,17 +112,18 @@ class Table (columnNames: Line, tabular: List[List[String]]) {
 
     val mergedColumns = (getColumnNames ++ other.getColumnNames).distinct
 
-    val mergedRows = getTabular.flatMap { row1 =>
+    val mergedRows = getTabular flatMap { row1 =>
       other.getTabular
-        .filter(row2 => row1(getColumnNames.indexOf(key)) == row2(other.getColumnNames.indexOf(key)))
+        .filter(row2 => row1(getColumnNames indexOf key) == row2(other.getColumnNames  indexOf key))
         .map { row2 =>
-          mergedColumns.map { col =>
+          mergedColumns map { col =>
             val value1 = row1
-                          .lift(      getColumnNames.indexOf(col))
+                          .lift(      getColumnNames indexOf col)
                           .getOrElse("")
             val value2 = row2
-                          .lift(other.getColumnNames.indexOf(col))
+                          .lift(other.getColumnNames indexOf col)
                           .getOrElse("")
+
             Seq(value1, value2)
               .filter(_.nonEmpty)
               .distinct
@@ -131,29 +135,31 @@ class Table (columnNames: Line, tabular: List[List[String]]) {
     val      tabularRows = getTabular
                           .filterNot(row1 => other.getTabular
                             .exists(row2 =>
-                              row1(getColumnNames.indexOf(key))
+                              row1(getColumnNames indexOf key)
                                 ==
-                                row2(other.getColumnNames.indexOf(key))))
+                                row2(other.getColumnNames indexOf key)))
+
     val otherTabularRows = other.getTabular
                           .filterNot(row2 => getTabular
                             .exists(row1 =>
-                              row2(other.getColumnNames.indexOf(key))
+                              row2(other.getColumnNames indexOf key)
                                 ==
-                                row1(getColumnNames.indexOf(key))))
+                                row1(getColumnNames indexOf key)))
 
-    val      difTabularRows =      tabularRows.map(
-      row1 => mergedColumns.map(
+    val      difTabularRows =      tabularRows map(
+      row1 => mergedColumns map(
         col => row1
-          .lift(getColumnNames.indexOf(col))
+          .lift(getColumnNames indexOf col)
           .getOrElse("")))
 
-    val difOtherTabularRows = otherTabularRows.map(
-      row2 => mergedColumns.map(
+    val difOtherTabularRows = otherTabularRows map(
+      row2 => mergedColumns map(
         col => row2
-          .lift(other.getColumnNames.indexOf(col))
+          .lift(other.getColumnNames indexOf col)
           .getOrElse("")))
 
     val mergedTable = new Table(mergedColumns, mergedRows ++ difTabularRows ++ difOtherTabularRows)
+
     Option(mergedTable)
   }
 }
